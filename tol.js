cf1518887f4642f195fc3e07b7d6603212428580
Vlad Irimus <you@example.com>
2019-01-06 15:00:31 +0200

    new

---
+++ tol.js
@@ -0,0 +1,66 @@
+// 1. Given an array of N distinct natural numbers, how many pairs of numbers sum up to a given number S?
+  const pairCounter = (arr, S) => {
+  let results = [];
+  for (let n=0; n<arr.length; n++) {
+    for (let j=n+1; j<arr.length; j++) {
+      if (arr[j] === S - arr[n]) {
+        results.push([arr[n], arr[j]])
+      }
+    }
+  }
+  return results.length;
+}
+
+pairCounter([-1, 2, 3, 4, 5, 6, 7, 8], 7);
+
+const pairCounter = function (arr, S) {
+  let res = 0;
+  arr.forEach((item) => {
+    if (arr.includes(S - item)) {
+      arr[arr.indexOf(item)] = null;
+      res++;
+    };
+  });
+  return res;
+}
+
+pairCounter([-1, 2, 3, 4, 5, 6, 7, 8], 7);
+
+// Given an array of N natural numbers, find the number that appears more than N/2 times.
+
+const findNum = (arr) => {
+	const pairs = [];
+	 arr.sort().reduce((ini, num) => {
+		return [...ini, [num, 1]]
+	}, []).forEach((item) => {
+		if (pairs.length === 0) {
+			pairs.push(item)
+		};
+		if (pairs.length === 0 || pairs[0][0] === item[0]) {
+			pairs[0][1]++
+		} else {
+			pairs.unshift(item)
+		}
+	});
+	return pairs;
+}
+
+findNum([1, 1, 2, 3, 6, 5, 4, 4, 4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
